0c8a34d30e86dc07b2ab156b1dda28ed629ab073
rodrigofontes15 <rodrigofontes15@users.noreply.github.com>
2020-02-23 17:52:58 +0100

    Criadas CRUD APIs para Tasks

---
+++ TaskMaster/Controllers/Api/TasksController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskMaster.Models;
+
+namespace TaskMaster.Controllers.Api
+{
+    public class TasksController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public TasksController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET /api/projetos
+        public IEnumerable<Tasks> GetTasks()
+        {
+            return _context.Tasks.ToList();
+        }
+
+        // GET /api/projetos/id
+        public Tasks GetTask(int id)
+        {
+            var task = _context.Tasks.SingleOrDefault(c => c.TasksId == id);
+
+            if (task == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return task;
+        }
+
+        // POST /api/projetos/
+        public Tasks PostTask(Tasks task)
+        {
+           if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+
+            return task;
+        }
+
+        // PUT /api/projetos/id
+        [HttpPut]
+        public void UpdateTask(int id, Tasks tasks)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var taskInDb = _context.Tasks.SingleOrDefault(c => tasks.TasksId == id);
+            if (taskInDb==null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            taskInDb.NomeTask = tasks.NomeTask;
+            taskInDb.DataInicio = tasks.DataInicio;
+            taskInDb.DataEstimada = tasks.DataEstimada;
+            taskInDb.TestersId = tasks.TestersId;
+            taskInDb.ProjetosId = tasks.ProjetosId;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/tasks/
+        [HttpDelete]
+        public void DeleteTask(int id)
+        {
+            
+            var taskInDb = _context.Tasks.SingleOrDefault(c => c.TasksId == id);
+            if (taskInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Tasks.Remove(taskInDb);
+            _context.SaveChanges();
+        }
+
+    }
+}
