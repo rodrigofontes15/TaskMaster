fad8319de83c8cc1508b69be1544b2eca8252ea9
rodrigofontes15 <rodrigofontes15@users.noreply.github.com>
2020-03-11 17:41:14 +0100

    correcao bug: ao deletar Bug, DataReal do projeto nao atualizava Atualizacao de Views Index de Projetos

--- TaskMaster/Controllers/Api/BugsController.cs
+++ TaskMaster/Controllers/Api/BugsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -38,7 +39,7 @@ namespace TaskMaster.Controllers.Api
         // POST /api/bugs/
         public Bugs PostBugs(Bugs bug)
         {
-           if (!ModelState.IsValid)
+            if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
             _context.Bugs.Add(bug);
@@ -56,7 +57,7 @@ namespace TaskMaster.Controllers.Api
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
             var bugInDb = _context.Bugs.SingleOrDefault(c => bugs.BugsId == id);
-            if (bugInDb==null)
+            if (bugInDb == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
             bugInDb.DescBug = bugs.DescBug;
@@ -73,14 +74,39 @@ namespace TaskMaster.Controllers.Api
         [HttpDelete]
         public void DeleteBug(int id)
         {
-            
+
+            var taskdobug = _context.Bugs.Where(p => p.BugsId == id).Select(t => t.TasksId).SingleOrDefault();
+            var projetidnatask = _context.Tasks.Where(t => t.TasksId == taskdobug).Select(p => p.ProjetosId).SingleOrDefault();
+            var dataInDbProjetos = _context.Projetos.Where(p => p.ProjetosId == projetidnatask).Select(d => d.DataReal).SingleOrDefault();
+            var databugInDb = _context.Bugs.Where(c => c.BugsId == id).Select(d => d.DataEstimadaBug).SingleOrDefault();
             var bugInDb = _context.Bugs.SingleOrDefault(c => c.BugsId == id);
+
+            var DataNull = "";
+
             if (bugInDb == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
+            else
+            {
+                if (databugInDb == dataInDbProjetos)
+                {
+                    var sqlDataRealTask = @"Update [Projetos] SET DataReal = @DataReal WHERE ProjetosId = @ProjetosId";
 
-            _context.Bugs.Remove(bugInDb);
-            _context.SaveChanges();
+                    _context.Database.ExecuteSqlCommand(
+                        sqlDataRealTask,
+                        new SqlParameter("@DataReal", DataNull),
+                        new SqlParameter("@ProjetosId", projetidnatask));
+
+                    _context.Bugs.Remove(bugInDb);
+                    _context.SaveChanges();
+                }
+                else
+                {
+
+                    _context.Bugs.Remove(bugInDb);
+                    _context.SaveChanges();
+                }
+            }
+
         }
-
     }
 }
